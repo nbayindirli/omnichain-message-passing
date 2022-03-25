a31b2188f7594b62475b4faa91407d7a772e9a53
Noah Bayindirli 🥂 <noahbay7@gmail.com>
2022-03-24 21:20:23 -0700

    Add fuji, rinkeby, and mumbai networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,34 +9,42 @@ import "solidity-coverage";
 
 dotenv.config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     const accounts = await hre.ethers.getSigners();
 
     for (const account of accounts) {
-            console.log(account.address);
+        console.log(account.address);
     }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
-    solidity: "0.8.4",
+    solidity: "0.8.10",
     networks: {
-            ropsten: {
-            url: process.env.ROPSTEN_URL || "",
+        fuji: {
+            url: "https://api.avax-test.network/ext/bc/C/rpc",
             accounts:
-                process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-            },
+                process.env.MASK_PRIVATE_KEY_A1 !== undefined ? [`0x${process.env.MASK_PRIVATE_KEY_A1}`] : [],
+            chainId: 43113,
+            gasPrice: 225000000000
+        },
+        rinkeby: {
+            url: process.env.ALCHEMY_PREP_URL_RINKEBY || "",
+            accounts:
+                process.env.MASK_PRIVATE_KEY_A1 !== undefined ? [process.env.MASK_PRIVATE_KEY_A1] : [],
+        },
+        mumbai: {
+            url: "https://rpc-mumbai.maticvigil.com",
+            accounts:
+                process.env.MASK_PRIVATE_KEY_A1 !== undefined ? [process.env.MASK_PRIVATE_KEY_A1] : [],
+            gasPrice: 8000000000
+        }
     },
     gasReporter: {
-            enabled: process.env.REPORT_GAS !== undefined,
-            currency: "USD",
+        enabled: true,
+        currency: "USD",
     },
     etherscan: {
-            apiKey: process.env.ETHERSCAN_API_KEY,
+        apiKey: process.env.ETHERSCAN_PREP_API_KEY,
     },
 };
 
